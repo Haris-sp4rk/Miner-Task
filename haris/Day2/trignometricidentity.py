da2b0db5a898e8af63c93ede8aeb024af1f76422
Haris Aqeel <k190249@nu.edu.pk>
2022-06-21 13:28:46 +0500

    Day2 Task Completed Haris

---
+++ haris/Day2/trignometricidentity.py
@@ -0,0 +1,10 @@
+import math 
+
+def trignometricidentity():
+    x =int(input("Enter a value to calculate trignometric identitities:"))
+    print("Value of sin is::",math.sin(x))
+    print("Value of cos is::",math.cos(x))
+    print("Value of tan is::",math.tan(x))
+
+
+    
