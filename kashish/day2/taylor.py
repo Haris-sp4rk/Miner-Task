cf709fa8a51feecd02a912e66d12b1458ddce26c
Kashish Fatima <kashish8430@gmail.com>
2022-06-21 17:37:52 +0500

    testcases

--- kashish/day2/taylor.py
+++ kashish/day2/taylor.py
@@ -1,14 +1,21 @@
-def taylorExpansion(x, numOfTerms):
+def taylorExpansion():
 
-    fact = 1
-    sum = 0.0
-
-    for i in range(1, numOfTerms):
-        fact= fact * i
-        sum = sum+(pow(x, i)/fact)
-
-    sum = sum + 1
-    return sum
+    try:
+        x = int(input("Enter value of x:"))
+        numOfTerms = int(input("Enter number of terms: "))
 
 
-# print(taylorExpansion(2, 10))
+        fact = 1
+        sum = 0.0
+
+        for i in range(1, numOfTerms):
+            fact= fact * i
+            sum = sum+(pow(x, i)/fact)
+
+        sum = sum + 1
+        return sum
+    except:
+        print("Invalid Input. Please try again.")
+
+
+print(taylorExpansion())
