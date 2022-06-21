5114edcc2d3aea84ffb41e721c1ce07baa10963f
Haris Aqeel <k190249@nu.edu.pk>
2022-06-21 14:59:29 +0500

    DAy1 update nt tehteer

--- Tatheer/Day1/LoanPayment.js
+++ Tatheer/Day1/LoanPayment.js
@@ -1,6 +1,6 @@
 var a;
-const p = parseInt(prompt('Enter your loan amount : '));
-const r = parseInt(prompt('Enter nterest rate per period : '));
-const n = parseInt(prompt('Enter total number of periods :  '));
-a=p(r (1+r)^n) / ( (1+r)^n -1 );
-console.log("Payment amount per period : " + a);
+var p= prompt('Enter your loan amount : ');
+var r = prompt('Enter nterest rate per period : ');
+var n = prompt('Enter total number of periods :  ');
+a=p*(r*(1+r)^n) / ( (1+r)^n -1 );
+alert("Payment amount per period : " + a);
