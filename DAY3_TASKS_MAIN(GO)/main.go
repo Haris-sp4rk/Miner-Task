931d6dfc1c54679ed0fb3c6fcfff1371410142aa
Haris Aqeel <k190249@nu.edu.pk>
2022-06-22 15:15:19 +0500

    added Day3 tasks

---
+++ DAY3_TASKS_MAIN(GO)/main.go
@@ -0,0 +1,37 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import "fmt"
+//import "test"
+func main() {
+	fmt.Println("Select from the options what u want to use:")
+	fmt.Println("1.Trignometric Identities ")
+	fmt.Println("2.Taylor Expansion")
+	fmt.Println("3.Binomial Coefficient")
+	fmt.Println("4.Loan Payment")
+	fmt.Println("5.Gaussian")
+
+	
+	fmt.Println("Select Any:")
+	
+	var x int
+	fmt.Scanln(&x)
+    
+	if(x==1):
+		Trignometricidentity()
+	elif(x==2):
+		TaylorExpansion()
+	elif(x==3):
+		Binomial()
+	elif(x==4):
+		LoanPayment()
+	elif(x==5):
+		Gaussain()
+	else:
+		fmt.Println("Wrong Input")
+
+	fmt.Println("Hello, 世界")
+	Haris()
+}
+      
